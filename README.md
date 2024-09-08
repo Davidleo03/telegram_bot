2c55cc15f5a9a57c8d79b9085147253e36e914d9
David López <133236822+Davidleo03@users.noreply.github.com>
2024-09-08 23:34:25 +0000

    version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# telegram_bot
+# OmmiEye Bot
+
+OmmiEye es un bot de Telegram que utiliza Hugging Face para describir imágenes enviadas por los usuarios y traducir las descripciones del inglés al español. 
+
+## Características
+- Recibe imágenes de los usuarios.
+- Utiliza el modelo Salesforce/blip-image-captioning-large de Hugging Face para generar descripciones en inglés de las imágenes.
+- Traduce las descripciones al español utilizando el modelo Helsinki-NLP/opus-mt-en-es.
+- Responde al usuario con la descripción traducida de la imagen.
+
+## Requisitos
+
+- Node.js 14 o superior.
+- Una cuenta en [Hugging Face](https://huggingface.co/) con acceso a la API de Inference.
+- Un bot de Telegram configurado (con el token del bot).
+
+## Instalación
+
+.1 Ejecuta los siguientes comandos
+    npm i @huggingface/inference telegraf
+
+    npm i nodemon -D
+
+.2 Inicia el servidor
+    
+    npm run dev
+
+    
+
---
+++ src/main.js
@@ -0,0 +1,51 @@
+import { HfInference } from "@huggingface/inference";
+import { Telegraf } from "telegraf";
+
+
+const hf = new HfInference('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');
+const bot = new Telegraf('7281464980:AAGc3PyU1MCdrmt40W2AScXEgMtP9-D9fis');
+
+bot.start(ctx => ctx.reply(`Hola ${ctx.message.chat.first_name} soy OmmiEye`))
+
+
+
+// Recibe las imágenes enviadas por el usuario
+bot.on('photo', async (ctx) => {
+    try {
+        const fileId = ctx.message.photo.pop().file_id;
+        const fileUrl = await ctx.telegram.getFileLink(fileId);
+
+
+
+       const response = await fetch(fileUrl);
+       const blob = await response.blob();
+
+       // Envía la imagen a Hugging Face para descripción
+       const description = await hf.imageToText({
+           data: blob,
+           model: 'Salesforce/blip-image-captioning-large'
+        });
+
+       // Envía la descripción al usuario
+       if (description) {  
+           const { generated_text } = description;
+           const {translation_text} = await hf.translation({
+            model: 'Helsinki-NLP/opus-mt-en-es',
+            inputs: generated_text
+          })
+          console.log(translation_text)
+    
+        ctx.reply(translation_text)   
+        } else {
+            ctx.reply('Lo siento, no pude describir la imagen.');
+        }  
+    } catch (error) {
+        console.log(error);
+        ctx.reply('Ha ocurrido un error')
+    }       
+});
+
+// Inicia el bot
+bot.launch();
+
+console.log('Bot está funcionando...');
