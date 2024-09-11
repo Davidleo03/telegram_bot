c6a6a926394e6a7a6d4d5231d01e52667006eb14
David López <133236822+Davidleo03@users.noreply.github.com>
2024-09-11 17:46:15 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,12 +8,13 @@ app.use(bot.webhookCallback('/bot'))
 
 app.get('/', (req, res) => res.send('Servidor Corriendo'));
 
-bot.telegram.setWebhook('https://telegram-bot-1-kbv2.onrender.com');
+
 
 const port = 3000 || process.env.PORT;
 
 app.listen(port, () => console.log('Server Active'))
 
+bot.telegram.setWebhook('https://telegram-bot-1-kbv2.onrender.com');
 
 
 console.log('Bot está funcionando...');
